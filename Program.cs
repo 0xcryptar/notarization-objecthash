0b967332488c7f877e00e1b7dc8e6768d45e3ca0
chuongle-nfq <37998997+chuongle-nfq@users.noreply.github.com>
2019-01-15 15:39:57 +0700

    dont redirect to 5001

--- Program.cs
+++ Program.cs
@@ -19,6 +19,7 @@ namespace ObjectHashServer
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseUrls("http://localhost:5000")
                 .UseStartup<Startup>();
     }
 }
