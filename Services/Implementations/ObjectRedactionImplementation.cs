d3c7ac0691ba5193be22224ac9eb6172b873f9de
Florian Weigand <florian.weigand@gmx.de>
2019-03-13 18:57:16 +0100

    add DSL implementation for READCT:forEach and REDACT:ifObjectContains

--- Services/Implementations/ObjectRedactionImplementation.cs
+++ Services/Implementations/ObjectRedactionImplementation.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json.Linq;
 using ObjectHashServer.Exceptions;
 using ObjectHashServer.Models.Extensions;
@@ -37,6 +38,13 @@ namespace ObjectHashServer.Services.Implementations
                 case JTokenType.Object:
                     try
                     {
+                        List<string> objectKeys = ((JObject)redactSettings).Properties().Select(p => p.Name).ToList();
+                        // a valid REDACT DSL command is an object with exactly one element where the this key starts with "REDACT"
+                        if (objectKeys.Count == 1 && objectKeys[0].StartsWith("REDACT", Globals.STRING_COMPARE_METHOD)) // TODO: CHECK: && json.Type == JTokenType.Array)
+                        {
+                            return RedactJTokenWithCommand(json, (JObject)redactSettings, salts.IsNullOrEmpty() ? null : salts);
+                        }
+
                         return RedactObject((JObject)json, (JObject)redactSettings, salts.IsNullOrEmpty() ? null : (JObject)salts);
                     }
                     catch (InvalidCastException e)
@@ -115,6 +123,76 @@ namespace ObjectHashServer.Services.Implementations
             }
 
             return (json, salts);
+        }
+
+        private (JToken json, JToken salts) RedactJTokenWithCommand(JToken json, JObject command, JToken salts = null)
+        {
+            if (command.ContainsKey("REDACT:forEach"))
+            {
+                return RedactArrayForEach((JArray)json, command, salts);
+            }
+
+            if (command.ContainsKey("REDACT:ifObjectContains"))
+            {
+                return RedactIfObjectContains((JObject)json, command, salts);
+            }
+
+            // TODO: there are only two DSL commands at the moment. More DSL commands can be added over time
+
+            IDictionary additionalExceptionData = new Dictionary<string, object>
+            {
+                { "commandObject", command }
+            };
+
+            throw new BadRequestException("You tried to use a redact command. The command you used is not valid. Currently available: 'REDACT:forEach'", additionalExceptionData);
+        }
+
+        /// <summary>
+        /// will apply the redact settings provided for each element of an array
+        /// forces the JSON object to by of type array
+        /// </summary>
+        /// <returns>The array for each.</returns>
+        /// <param name="json">Json.</param>
+        /// <param name="command">Command.</param>
+        /// <param name="salts">Salts.</param>
+        private (JToken json, JToken salts) RedactArrayForEach(JArray json, JObject command, JToken salts = null)
+        {
+            for (int i = 0; i < json.Count; i++)
+            {
+                if(salts.IsNullOrEmpty())
+                {
+                    (json[i], _) = RecursivlyRedactDataAndSalts(json[i], command["REDACT:forEach"], null);
+                }
+                else
+                {
+                    (json[i], salts[i]) = RecursivlyRedactDataAndSalts(json[i], command["REDACT:forEach"], salts);
+
+                }
+            }
+
+            return (json, salts);
+        }
+
+        /// <summary>
+        /// will apply the redact the object only if the object contains the values provided
+        /// </summary>
+        /// <returns>The if object contains.</returns>
+        /// <param name="json">Json.</param>
+        /// <param name="command">Command.</param>
+        /// <param name="salts">Salts.</param>
+        private (JToken json, JToken salts) RedactIfObjectContains(JObject json, JObject command, JToken salts = null)
+        {
+            JObject obj = (JObject)command["REDACT:ifObjectContains"];
+
+            foreach (var o in obj)
+            {
+                if (!json.ContainsKey(o.Key) || !JToken.DeepEquals(json[o.Key], obj[o.Key]))
+                {
+                    return (json, salts);
+                }
+            }
+
+            return RecursivlyRedactDataAndSalts(json, true, salts);
         }
     }
 }
