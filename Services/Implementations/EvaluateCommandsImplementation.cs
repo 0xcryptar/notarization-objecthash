a77b0e4b51773412a8c864d3d5d164ee32be6160
Florian Weigand <florian.weigand@gmx.de>
2019-03-22 09:45:53 +0100

    add 'add' and 'or' command in redact commands, finish refactoring commands to own implementation'

---
+++ Services/Implementations/EvaluateCommandsImplementation.cs
@@ -0,0 +1,159 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json.Linq;
+using ObjectHashServer.Exceptions;
+
+namespace ObjectHashServer.Services.Implementations
+{
+    public static class EvaluateCommandsImplementation
+    {
+        /// <summary>
+        /// If the redact settings contain command objects they will be evaluated here.
+        /// Result is a redact setting containing only booleans.
+        /// This method does change the provided inputs. So there is no need to clone
+        /// them before calling this method.
+        /// </summary>
+        /// <returns>The fully evaluated redact settings object so that all JSON leaves
+        /// only contain a boolean</returns>
+        public static JToken EvaluateCommands(JToken redactSettings, JToken json)
+        {
+            return RecursiveEvaluateCommands(redactSettings.DeepClone(), json.DeepClone());
+        }
+
+        private static JToken RecursiveEvaluateCommands(JToken redactSettings, JToken json)
+        {
+            switch (redactSettings.Type)
+            {
+                case JTokenType.Array:
+                    return RecursiveEvaluateJArray((JArray) redactSettings, json);
+                case JTokenType.Object:
+                    List<string> objectKeys = ((JObject) redactSettings).Properties().Select(p => p.Name).ToList();
+                    if (objectKeys.Count == 1 && objectKeys[0].StartsWith("REDACT", Globals.STRING_COMPARE_METHOD))
+                    {
+                        return EvaluateSingleCommand(objectKeys[0], (JObject) redactSettings, json);
+                    }
+                    else
+                    {
+                        return RecursiveEvaluateJObject((JObject) redactSettings, json);
+                    }
+                case JTokenType.Boolean:
+                    return redactSettings;
+                default:
+                    throw new BadRequestException(
+                        "The redact settings are invalid. Commands can only be of type JSON object.");
+            }
+        }
+
+        private static JToken RecursiveEvaluateJArray(JArray redactSettings, JToken json)
+        {
+            JArray result = new JArray();
+            for (int i = 0; i < redactSettings.Count; i++)
+            {
+                result.Add(RecursiveEvaluateCommands(redactSettings[i], json[i]));
+            }
+
+            return result;
+        }
+
+        private static JToken RecursiveEvaluateJObject(JObject redactSettings, JToken json)
+        {
+            JObject result = new JObject();
+            foreach ((string key, JToken jToken) in redactSettings)
+            {
+                result[key] = RecursiveEvaluateCommands(jToken, json[key]);
+            }
+
+            return result;
+        }
+
+        private static JToken EvaluateSingleCommand(string command, JObject redactSettings, JToken json)
+        {
+            switch (command)
+            {
+                case "REDACT:forEach":
+                    try
+                    {
+                        return GenerateArrayWithForEachCommand((JObject) redactSettings["REDACT:forEach"],
+                            (JArray) json);
+                    }
+                    catch (InvalidCastException)
+                    {
+                        throw new BadRequestException(
+                            "You have tried to apply an forEach command. But the corresponding JSON was not an array.");
+                    }
+                case "REDACT:ifObjectContains":
+                    try
+                    {
+                        return RedactIfObjectContains((JObject) redactSettings["REDACT:ifObjectContains"],
+                            (JObject) json);
+                    }
+                    catch (InvalidCastException)
+                    {
+                        throw new BadRequestException(
+                            "You have tried to apply the ifObjectContains function. But the corresponding JSON is not an object.");
+                    }
+                case "REDACT:or":
+                    try
+                    {
+                        return RedactOr((JArray) redactSettings["REDACT:or"], json);
+                    }
+                    catch (InvalidCastException)
+                    {
+                        throw new BadRequestException(
+                            "You have tried to apply the or-Redact function. Please add the or commands as an array.");
+                    }
+                case "REDACT:and":
+                    try
+                    {
+                        return RedactAnd((JArray) redactSettings["REDACT:and"], json);
+                    }
+                    catch (InvalidCastException)
+                    {
+                        throw new BadRequestException(
+                            "You have tried to apply the and-Redact function. Please add the and commands as an array.");
+                    }
+                default:
+                    IDictionary additionalExceptionData = new Dictionary<string, object>
+                    {
+                        {"command", command}
+                    };
+
+                    throw new BadRequestException(
+                        "You have tried to use a redact command. The command you used is not valid. Currently available: 'REDACT:forEach'",
+                        additionalExceptionData);
+            }
+        }
+
+        private static JArray GenerateArrayWithForEachCommand(JObject redactSettings, JArray json)
+        {
+            JArray result = new JArray();
+
+            foreach (JToken subJson in json)
+            {
+                result.Add(RecursiveEvaluateCommands(redactSettings, subJson));
+            }
+
+            return result;
+        }
+
+        private static bool RedactIfObjectContains(JObject redactSettings, JObject json)
+        {
+            return redactSettings.Properties().Select(p => p.Name).All(key =>
+                json.ContainsKey(key) && JToken.DeepEquals(json[key], redactSettings[key]));
+        }
+
+        private static bool RedactOr(JArray orCommands, JToken json)
+        {
+            return orCommands.Select((t, i) => RecursiveEvaluateCommands((JObject) t, json[i]))
+                .Any(eval => (bool) eval);
+        }
+
+        private static bool RedactAnd(JArray andCommands, JToken json)
+        {
+            return andCommands.Select((t, i) => RecursiveEvaluateCommands((JObject) t, json[i]))
+                .All(eval => (bool) eval);
+        }
+    }
+}
--- Services/Implementations/ObjectRedactionImplementation.cs
+++ Services/Implementations/ObjectRedactionImplementation.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using Newtonsoft.Json.Linq;
 using ObjectHashServer.Exceptions;
 using ObjectHashServer.Models.Extensions;
+
 // ReSharper disable PossibleNullReferenceException
 
 namespace ObjectHashServer.Services.Implementations
@@ -22,66 +23,74 @@ namespace ObjectHashServer.Services.Implementations
         /// </summary>
         public static (JToken json, JToken salts) RedactJToken(JToken json, JToken redactSettings, JToken salts = null)
         {
-            return RecursiveRedactDataAndSalts(json.DeepClone(), redactSettings.DeepClone(), salts.IsNullOrEmpty() ? null : salts.DeepClone());
+            JToken evaluatedRedactSettings = EvaluateCommandsImplementation.EvaluateCommands(redactSettings, json);
+            return RecursiveRedactDataAndSalts(json.DeepClone(), evaluatedRedactSettings,
+                salts.IsNullOrEmpty() ? null : salts.DeepClone());
         }
 
-        private static (JToken json, JToken salts) RecursiveRedactDataAndSalts(JToken json, JToken redactSettings, JToken salts = null)
+        private static (JToken json, JToken salts) RecursiveRedactDataAndSalts(JToken json, JToken redactSettings,
+            JToken salts = null)
         {
-            // evaluate redact settings if they are of type object
-            JToken evaluateRedactSettings =
-                EvaluateCommandsInRedactSettingsImplementation.EvaluateCommandsInRedactSettings(redactSettings, json);
-            
             // ReSharper disable once SwitchStatementMissingSomeCases
-            switch (evaluateRedactSettings.Type)
+            switch (redactSettings.Type)
             {
                 case JTokenType.Boolean:
                     if (!(bool) redactSettings) return (json, salts);
-                    
+
                     ObjectHashImplementation objectHash = new ObjectHashImplementation();
                     objectHash.HashJToken(json, salts);
                     return ("**REDACTED**" + objectHash.HashAsString(), "**REDACTED**");
                 case JTokenType.Object:
                     try
                     {
-                        return RedactObject((JObject)json, (JObject)evaluateRedactSettings, salts.IsNullOrEmpty() ? null : (JObject)salts);
+                        return RedactObject((JObject) json, (JObject) redactSettings,
+                            salts.IsNullOrEmpty() ? null : (JObject) salts);
                     }
                     catch (InvalidCastException e)
                     {
-                        throw new BadRequestException("The provided JSON does not contain an object -> {} where the redact settings require one. Please check the JSON data or the redact settings.", e);
+                        throw new BadRequestException(
+                            "The provided JSON does not contain an object -> {} where the redact settings require one. Please check the JSON data or the redact settings.", e);
                     }
                 case JTokenType.Array:
-                    try {
-                        return RedactArray((JArray)json, (JArray)evaluateRedactSettings, salts.IsNullOrEmpty() ? null : (JArray)salts);
-                    }
-                    catch(InvalidCastException e)
+                    try
                     {
-                        throw new BadRequestException("The provided JSON does not contain an array -> [] where the redact settings require one. Please check the JSON data or the redact settings", e);
+                        return RedactArray((JArray) json, (JArray) redactSettings,
+                            salts.IsNullOrEmpty() ? null : (JArray) salts);
+                    }
+                    catch (InvalidCastException e)
+                    {
+                        throw new BadRequestException(
+                            "The provided JSON does not contain an array -> [] where the redact settings require one. Please check the JSON data or the redact settings", e);
                     }
                 case JTokenType.Null:
-                    {
-                        return (json, salts);
-                    }
+                {
+                    return (json, salts);
+                }
                 default:
-                    throw new BadRequestException("The redact setting JSON is invalid. It can only contain a nested JSON, arrays and the data type Boolean.");
+                    throw new BadRequestException(
+                        "The redact setting JSON is invalid. It can only contain a nested JSON, arrays and the data type Boolean.");
             }
         }
 
-        private static (JToken json, JToken salts) RedactObject(JObject json, JObject redactSettings, JObject salts = null)
+        private static (JToken json, JToken salts) RedactObject(JObject json, JObject redactSettings,
+            JObject salts = null)
         {
             foreach ((string key, JToken _) in redactSettings)
             {
                 if (!json.ContainsKey(key) || (!salts.IsNullOrEmpty() && !salts.ContainsKey(key)))
                 {
                     IDictionary additionalExceptionData = new Dictionary<string, object>
-                        {
-                            { "missingKey", key },
-                            { "errorInObject", !json.ContainsKey(key) ? "json" : "salts" }
-                        };
+                    {
+                        {"missingKey", key},
+                        {"errorInObject", !json.ContainsKey(key) ? "json" : "salts"}
+                    };
 
-                    throw new BadRequestException("The provided JSON or Salt defines an object which is different from the redact settings object. Please check the JSON, the salt data or the redact settings.", additionalExceptionData);
+                    throw new BadRequestException(
+                        "The provided JSON or Salt defines an object which is different from the redact settings object. Please check the JSON, the salt data or the redact settings.",
+                        additionalExceptionData);
                 }
 
-                if(salts.IsNullOrEmpty())
+                if (salts.IsNullOrEmpty())
                 {
                     (json[key], _) = RecursiveRedactDataAndSalts(json[key], redactSettings[key]);
                 }
@@ -93,21 +102,24 @@ namespace ObjectHashServer.Services.Implementations
 
             return (json, salts);
         }
+
         private static (JToken json, JToken salts) RedactArray(JArray json, JArray redactSettings, JArray salts = null)
         {
             if (redactSettings.Count != json.Count || (!salts.IsNullOrEmpty() && salts.Count != json.Count))
             {
                 IDictionary additionalExceptionData = new Dictionary<string, object>
-                    {
-                        { "errorInObject", redactSettings.Count == json.Count ? "json" : "salts" }
-                    };
+                {
+                    {"errorInObject", redactSettings.Count == json.Count ? "json" : "salts"}
+                };
 
-                throw new BadRequestException("The corresponding JSON or Salt object contains an array that is different in size from the redact settings array. They need to be equally long.", additionalExceptionData);
+                throw new BadRequestException(
+                    "The corresponding JSON or Salt object contains an array that is different in size from the redact settings array. They need to be equally long.",
+                    additionalExceptionData);
             }
 
             for (int i = 0; i < redactSettings.Count; i++)
             {
-                if(salts.IsNullOrEmpty())
+                if (salts.IsNullOrEmpty())
                 {
                     (json[i], _) = RecursiveRedactDataAndSalts(json[i], redactSettings[i]);
                 }
