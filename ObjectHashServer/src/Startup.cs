acade4e60b8704be2855c031ef2aed94b59b21d1
Florian Weigand <florian.weigand@gmx.de>
2020-12-15 16:37:48 +0100

    add hosting on bitflow.de and add ssl encryption

--- ObjectHashServer/src/Startup.cs
+++ ObjectHashServer/src/Startup.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
@@ -9,16 +10,23 @@ namespace ObjectHashServer
 {
     public class Startup
     {
+        private readonly IConfiguration  _configuration;
+        
         public Startup(IConfiguration configuration)
         {
-            Configuration = configuration;
+            _configuration = configuration;
         }
-
-        public IConfiguration Configuration { get; }
-
+        
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            
+            if (string.Equals(_configuration["SSL_STATUS"], "Enabled", StringComparison.CurrentCultureIgnoreCase))
+            {
+                // Add SSL encryption using Lets Encrypt
+                services.AddLettuceEncrypt();
+            }
+            
             services.AddMvc(config =>
             {
                 config.Filters.Add(new ExceptionHandlerFilterAttribute());
