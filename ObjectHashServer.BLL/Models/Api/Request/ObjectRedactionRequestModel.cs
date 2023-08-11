c1cf7f2c3f1043f8a07a2f03af3b7565c0b53c3d
tbordi <bordi.tamas@mirango.hu>
2023-08-11 16:17:39 +0300

    #9 Include redaction in the notarization hash API
    => adding redaction example object
    
    refs: #9

--- ObjectHashServer.BLL/Models/Api/Request/ObjectRedactionRequestModel.cs
+++ ObjectHashServer.BLL/Models/Api/Request/ObjectRedactionRequestModel.cs
@@ -1,8 +1,10 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Newtonsoft.Json.Linq;
 
 namespace ObjectHashServer.BLL.Models.Api.Request
 {
+    [OpenApiExample(typeof(ObjectRedactionRequestModelExample))]
     public class ObjectRedactionRequestModel : ObjectBaseRequestModel
     {
         [Required]
