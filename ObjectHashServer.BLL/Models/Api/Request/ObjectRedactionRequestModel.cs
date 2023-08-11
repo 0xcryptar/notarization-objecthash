afa50c584c50b90897a19d0b3526b51575e545ab
tbordi <bordi.tamas@mirango.hu>
2023-08-11 16:07:53 +0300

    Revert "#9 Include redaction in the notarization hash API"
    This reverts commit a39244bee64e89c8d81d77b4d08d1c695b25a950.

--- ObjectHashServer.BLL/Models/Api/Request/ObjectRedactionRequestModel.cs
+++ ObjectHashServer.BLL/Models/Api/Request/ObjectRedactionRequestModel.cs
@@ -1,10 +1,8 @@
 ﻿using System.ComponentModel.DataAnnotations;
-using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
 using Newtonsoft.Json.Linq;
 
 namespace ObjectHashServer.BLL.Models.Api.Request
 {
-    [OpenApiExample(typeof(ObjectRedactionRequestModelExample))]
     public class ObjectRedactionRequestModel : ObjectBaseRequestModel
     {
         [Required]
