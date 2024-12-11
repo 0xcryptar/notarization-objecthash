e2e78e9c0863240cceb3b8a97abf890d6812dcfc
SaroltaMihaly <mihaly.sarolta01@gmail.com>
2024-12-11 08:56:51 +0200

    CRY-49 | Added Sentry

--- ObjectHashServer.API/Program.cs
+++ ObjectHashServer.API/Program.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Newtonsoft.Json.Serialization;
 using Newtonsoft.Json;
+using Sentry;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
@@ -25,5 +26,22 @@ var host = new HostBuilder()
         services.AddMvcCore().AddNewtonsoftJson();
     })
     .Build();
+
+SentrySdk.Init(options =>
+{
+    // A Sentry Data Source Name (DSN) is required.
+    // See https://docs.sentry.io/product/sentry-basics/dsn-explainer/
+    // You can set it in the SENTRY_DSN environment variable, or you can set it in code here.
+    options.Dsn = "https://a15d97557f84d25bfbb417eef0bec23f@o4508133068963840.ingest.de.sentry.io/4508439116513360";
+
+    // When debug is enabled, the Sentry client will emit detailed debugging information to the console.
+    // This might be helpful, or might interfere with the normal operation of your application.
+    // We enable it here for demonstration purposes when first trying Sentry.
+    // You shouldn't do this in your applications unless you're troubleshooting issues with Sentry.
+    options.Debug = true;
+
+    // This option is recommended. It enables Sentry's "Release Health" feature.
+    options.AutoSessionTracking = true;
+});
 
 host.Run();
